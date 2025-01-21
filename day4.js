4777930873538e6568f1fc90301fa1a5cbcf376d
AnithaDaggula <anithareddy1215@gmail.com>
2025-01-21 22:03:55 +0530

    commit

---
+++ day4.js
@@ -0,0 +1,13 @@
+const hello="Hello world"
+const title="Card title"
+const subtitle="Card subtitle"
+const info="Some quick example text to build on the card title and make up the bulk of the card's content."
+const cardlink="Card link"
+const anotlink="Another link"
+
+console.log(hello)
+console.log(title)
+console.log(subtitle)
+console.log(info)
+console.log(cardlink)
+console.log(anotlink)
